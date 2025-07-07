974220ac0dec2e66826f1084177600b506826c9c
Arpit Neewaliya <arpit.neewaliya@gmail.com>
2025-07-07 17:45:49 +0530

    first commit

---
+++ routes/tasks.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const Task = require('../models/Task');
+
+// GET all tasks
+router.get('/', async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+// POST create a task
+router.post('/', async (req, res) => {
+  const newTask = new Task({ text: req.body.text });
+  await newTask.save();
+  res.json(newTask);
+});
+
+// PUT update (mark complete)
+router.put('/:id', async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  task.completed = !task.completed;
+  await task.save();
+  res.json(task);
+});
+
+// DELETE a task
+router.delete('/:id', async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Task deleted' });
+});
+
+module.exports = router;
